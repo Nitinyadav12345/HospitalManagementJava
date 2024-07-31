ebcad3c5c15498f9e16d9360cf7ed9ac6fd2a77a
NITIN YADAV <70438455+Nitinyadav12345@users.noreply.github.com>
2024-07-31 11:10:27 +0530

    Backend Intial commit

---
+++ Backend/src/test/java/com/app/HospitalManagement/HospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.HospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
