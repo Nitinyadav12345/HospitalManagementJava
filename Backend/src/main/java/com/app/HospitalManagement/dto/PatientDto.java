e031cdf527a196955bcdbe860394da6c7d461f92
NITIN YADAV <70438455+Nitinyadav12345@users.noreply.github.com>
2024-08-08 09:00:20 +0530

    add patient dto

---
+++ Backend/src/main/java/com/app/HospitalManagement/dto/PatientDto.java
@@ -0,0 +1,21 @@
+package com.app.HospitalManagement.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PatientDto {
+    private String bloodgroup;
+    private LocalDate dob;
+    private String prescription;
+    private String disease;
+    private Long userId;
+    private LocalDate doa;
+    private LocalDate dod;
+    private boolean isadmit;
+}
